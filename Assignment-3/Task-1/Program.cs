ba69fd37056b8eadd00961e85c2cc4fba6501f56
robiu hassan <victorshakil9102@gmail.com>
2021-03-04 00:03:41 +0600

    Assignment 3

---
+++ Assignment-3/Task-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task_1
+{
+    class Program
+    {
+        public delegate void Printsomething(int x, int y);
+
+        public static void Test(Printsomething a)
+        {
+            a(5, 5);
+        }
+        static void Main(string[] args)
+        {
+            Test(doprint);
+            
+        }
+
+        public static void doprint(int x, int y)
+        {
+            var v = x + y;
+            Console.WriteLine(v);
+        }
+    }
+}
---
+++ Assignment-3/Task-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How many ticket:");
+                var a=double.Parse(Console.ReadLine());
+                Console.WriteLine("Ticket price:");
+                var b = double.Parse(Console.ReadLine());
+                var c = a * b;
+                Console.WriteLine($"Price:{c}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+            finally
+            {
+                Console.WriteLine("Please visit us Again");
+            }
+        }
+    }
+}
