1351244779447efb8ffb69be73bfa20d1b6e2fbc
robiu hassan <victorshakil9102@gmail.com>
2021-03-04 02:53:44 +0600

    assignment 3

--- Assignment-3/Task-1/Program.cs
+++ Assignment-3/Task-1/Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Task_1
 {
     class Program
     {
-        public delegate void Printsomething(int x, int y);
-
-        public static void Test(Printsomething a)
-        {
-            a(5, 5);
-        }
+        
         static void Main(string[] args)
         {
-            Test(doprint);
-            
-        }
+            var contacts = new List<Contact>()
+            {
+                new Contact{Email = "shakilvictor@gmail.com",Mobile = "01515263089"},
+                new Contact{Email = "robiul35-1663@diu.edu.bd",Mobile = "01521334084"},
+                new Contact{Email = "tysonmashuq@gmail.com",Mobile = "01521334086"},
+            };
 
-        public static void doprint(int x, int y)
-        {
-            var v = x + y;
-            Console.WriteLine(v);
+            Button button = new Button();
+
+            button.Clicked+=new Button.SendMessage(new EmailMessage().SendEmailMessage);
+            button.Clicked += new Button.SendMessage(new SMSMessage().SendSMSMessage);
+            button.Click(contacts);
         }
+        
     }
 }
--- Assignment-3/Task-2/Program.cs
+++ Assignment-3/Task-2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 
 namespace Task_2
 {
@@ -6,7 +9,28 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>
+            {
+                new Student{Name = "shakil",Age = 25},
+                new Student{Name = "victor",Age = 22},
+                new Student{Name = "salma",Age = 23},
+                new Student{Name = "riad",Age = 12},
+                new Student{Name = "Yousuf",Age = 25},
+                new Student{Name = "Mashuq",Age = 25},
+            };
+
+
+            var student1 = from student in students
+                where student.Age >= 20
+                orderby student.Name ascending
+               orderby student.Age descending
+               select student;
+            foreach (var stu in student1)
+            {
+                Console.WriteLine($"student name:{stu.Name},student age:{stu.Age}");
+            }
+
+
         }
     }
 }
